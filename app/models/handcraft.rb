173653c32c2fc1100a96fd57e052eeb84f10a65e
VanessaSouza142 <vanessa.kbs@hotmail.com>
2023-04-17 16:54:03 -0400

    Improve front-end details

--- app/models/handcraft.rb
+++ app/models/handcraft.rb
@@ -1,6 +1,6 @@
 class Handcraft < ApplicationRecord
     validates :name, presence: true
-    validates :description, presence: true, length: { minimum: 5, maximum: 500 }
+    validates :description, presence: true, length: { minimum: 5, maximum: 800 }
     belongs_to :artisan
     validates :artisan_id, presence: true
     default_scope -> { order(updated_at: :desc) }
